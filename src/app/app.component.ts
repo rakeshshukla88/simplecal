d9d75c5d3543b515eeefca2a286b0db2f3afe71d
rakesh.s@cisinlabs.com <rakesh.s@cisinlabs.com>
2020-11-28 10:05:41 +0530

    missed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simplecal';
+    title = 'simplecal';
+    calcSyntax='';
+    resultant='';
+    operandOne='';
+    operandOneDecimal=false;
+    operandTwo='';
+    operandTwoDecimal = false;
+    operator='';
+    operatorSelected=false;
+    
+    
+    
+    userInput(keyInput){
+        
+        if (this.operatorSelected){
+            if (keyInput == '.'){
+                if(this.operandOneDecimal==true){
+                    return false;
+                }
+                this.operandOneDecimal=true;
+            }
+            this.operandTwo += keyInput;
+        }else{
+            if (keyInput == '.') {
+                if (this.operandTwoDecimal == true) {
+                    return false;
+                }
+                this.operandTwoDecimal = true;
+            }
+            this.operandOne += keyInput;
+        }
+        this.calcSyntax +=keyInput;
+        
+    }
+    
+    mathFunction(operatorKey){
+        if (this.operatorSelected){
+            alert("invalid operation");
+            return false;
+        }
+        this.operatorSelected=true;
+        this.calcSyntax += operatorKey;
+        this.operator = operatorKey;
+    }
+    
+    generateresult(){
+        if (this.operandTwo==""){
+            alert("invalid operation");
+            return false;
+        }
+        this.resultant = eval(this.calcSyntax);
+        this.operandOne = '';
+        this.operandTwo = '';
+        this.operator = '';
+        this.operatorSelected = false;
+        this.operandOneDecimal = false;
+        this.operandTwoDecimal = false;
+    }
+    
+    clearScreen(type){
+        this.calcSyntax = '';
+        this.resultant = '';
+        this.operandOne = '';
+        this.operandTwo = '';
+        this.operator = '';
+        this.operatorSelected = false;
+        this.operandOneDecimal = false;
+        this.operandTwoDecimal = false;
+    }
 }
